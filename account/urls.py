7b7513737631f8b0aa2a9f136ea87ab4d59019ff
Roman <roman.u.marchenko@gmail.com>
2019-02-28 10:34:29 +0200

    new branch

--- account/urls.py
+++ account/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     # register user urls
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit')
 
 ]
