60ff6137dd301c48a4e1b480a2fb12274204a828
Roman <roman.u.marchenko@gmail.com>
2019-03-09 12:20:51 +0200

    new branch=)

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -135,5 +136,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
 ]
-
